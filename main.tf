45e991ab497cf9e52a77b4c067ea64ea3b41aeb7
tabbygo-arc <118011987+tabbygo-arc@users.noreply.github.com>
2023-10-09 17:01:15 -0400

    az2

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_availability_zones" "available" {}
+# data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
